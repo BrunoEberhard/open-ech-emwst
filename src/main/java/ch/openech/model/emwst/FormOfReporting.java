2cb3573b4a7248d7aac760c6059abd6f5bba3487
bruno <bruno.eberhard@pop.ch>
2017-05-25 15:07:14 +0200

    Enum implementieren EchCode

--- src/main/java/ch/openech/model/emwst/FormOfReporting.java
+++ src/main/java/ch/openech/model/emwst/FormOfReporting.java
@@ -1,7 +1,14 @@
 package ch.openech.model.emwst;
 
-public enum FormOfReporting {
+import ch.openech.model.types.EchCode;
+
+public enum FormOfReporting implements EchCode {
 
 	vereinbart, vereinnahmt;
+
+	@Override
+	public String getValue() {
+		return String.valueOf(this.ordinal() + 1);
+	}
 	
 }
--- src/main/java/ch/openech/model/emwst/TypeOfSubmission.java
+++ src/main/java/ch/openech/model/emwst/TypeOfSubmission.java
@@ -1,7 +1,14 @@
 package ch.openech.model.emwst;
 
-public enum TypeOfSubmission {
+import ch.openech.model.types.EchCode;
+
+public enum TypeOfSubmission implements EchCode {
 
 	Ersteinreichung, Korrekturabrechnung, Jahresabstimmung;
+	
+	@Override
+	public String getValue() {
+		return String.valueOf(this.ordinal() + 1);
+	}
 	
 }
