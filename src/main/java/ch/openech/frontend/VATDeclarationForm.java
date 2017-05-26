d7d61c4db581a48b0cd45690b5e1d15e5e2db0c8
bruno <bruno.eberhard@pop.ch>
2017-05-26 15:28:27 +0200

    VATDeclarationForm anpassungen

--- src/main/java/ch/openech/frontend/VATDeclarationForm.java
+++ src/main/java/ch/openech/frontend/VATDeclarationForm.java
@@ -7,25 +7,20 @@ import org.minimalj.util.resources.Resources;
 
 import ch.openech.model.emwst.EffectiveReportingMethod;
 import ch.openech.model.emwst.FlatTaxRateMethod;
-import ch.openech.model.emwst.GeneralInformation;
 import ch.openech.model.emwst.SendingApplication;
-import ch.openech.model.emwst.TurnoverComputation;
 
 public class VATDeclarationForm extends Form<VATDeclarationEditModel> {
 
 	public VATDeclarationForm(boolean editable, Class<?> reportingMethod) {
 		super(editable, 4);
 
-		addTitle(Resources.getString(GeneralInformation.class.getSimpleName()));
+		addTitle(Resources.getString("GeneralInformation"));
 		line($.generalInformation.uid.value, $.generalInformation.organisationName);
-		line($.generalInformation.generationTime);
 		line($.generalInformation.reportingPeriodFrom, $.generalInformation.reportingPeriodTill);
 		line($.generalInformation.typeOfSubmission, $.generalInformation.formOfReporting);
 		line($.generalInformation.businessReferenceId);
-		SendingApplication $_application = $.generalInformation.sendingApplication;
-		line($_application.product, $_application.productVersion, $_application.manufacturer);
 
-		addTitle(Resources.getString(TurnoverComputation.class.getSimpleName()));
+		addTitle(Resources.getString("TurnoverComputation"));
 		line($.turnoverComputation.totalConsideration);
 		line($.turnoverComputation.suppliesToForeignCountries, $.turnoverComputation.suppliesAbroad);
 		line($.turnoverComputation.transferNotificationProcedure);
@@ -38,22 +33,26 @@ public class VATDeclarationForm extends Form<VATDeclarationEditModel> {
 			line($.opted);
 		}
 		
+		addTitle(Resources.getString("SuppliesPerTaxRate"));
 		if (reportingMethod == FlatTaxRateMethod.class) {
 			line($.suppliesPerTaxRate0.taxRate, $.suppliesPerTaxRate0.turnover, $.suppliesPerTaxRate0.activity);
 			line($.suppliesPerTaxRate1.taxRate, $.suppliesPerTaxRate1.turnover, $.suppliesPerTaxRate1.activity);
-			line($.suppliesPerTaxRate2.taxRate, $.suppliesPerTaxRate2.turnover, $.suppliesPerTaxRate2.activity);
 		} else {
 			line($.suppliesPerTaxRate0.taxRate, $.suppliesPerTaxRate0.turnover);
 			line($.suppliesPerTaxRate1.taxRate, $.suppliesPerTaxRate1.turnover);
-			line($.suppliesPerTaxRate2.taxRate, $.suppliesPerTaxRate2.turnover);
 		}
 
+		addTitle(Resources.getString("AcquisitionTax"));
 		line($.acquisitionTax0.taxRate, $.acquisitionTax0.turnover);
 		line($.acquisitionTax1.taxRate, $.acquisitionTax1.turnover);
-		line($.acquisitionTax2.taxRate, $.acquisitionTax2.turnover);
 
 		addTitle("-");
 		line($.payableTax);
+		
+		addTitle(Resources.getString("TechnicalInformation"));
+		SendingApplication $_application = $.generalInformation.sendingApplication;
+		line($_application.product, $_application.productVersion, $_application.manufacturer, $.generalInformation.generationTime);
+
 	}
 	
 	@Override
