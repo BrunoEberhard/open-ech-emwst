ebaa0f9a6354146ae6ac1c67dee4c0f131f7819c
bruno <bruno.eberhard@pop.ch>
2017-06-03 10:03:03 +0200

    TurnoverTaxRate.getResult()
    Momentan noch nicht gebraucht

--- src/main/java/ch/openech/model/emwst/TurnoverTaxRate.java
+++ src/main/java/ch/openech/model/emwst/TurnoverTaxRate.java
@@ -2,12 +2,15 @@ package ch.openech.model.emwst;
 
 import java.math.BigDecimal;
 
+import org.minimalj.model.Keys;
 import org.minimalj.model.annotation.Decimal;
 import org.minimalj.model.annotation.NotEmpty;
 import org.minimalj.model.annotation.Size;
 
 public class TurnoverTaxRate {
 
+	public static final TurnoverTaxRate $ = Keys.of(TurnoverTaxRate.class);
+	
 	// only used in FlatTaxRateMethod
 	@Size(255) @NotEmpty
 	public String activity;
@@ -17,4 +20,18 @@ public class TurnoverTaxRate {
 	
 	@Decimal(2) @NotEmpty
 	public BigDecimal turnover;
+	
+	public String getResult() {
+		// In folgender Variante würde definiert, dass result von taxRate und turnover abhängt
+		// Vorläufig ist das in der nächten Version von Minimal-J drin, braucht aber noch etwas
+		// mehr Research, ob das wirklich eine gute Idee ist.
+		// if (Keys.isKeyObject(this)) return Keys.methodOf(this, "result", $.taxRate, $.turnover);
+
+		if (Keys.isKeyObject(this)) return Keys.methodOf(this, "result");
+		if (taxRate != null && turnover != null) {
+			return taxRate.multiply(turnover).divide(BigDecimal.valueOf(100)).toPlainString();
+		} else {
+			return null;
+		}
+	}
 }
